c9e34259c68c2fa1b3bbf371611c651bbcce43c4
Patrick Springstubbe <pat@springstubbe.us>
2017-07-21 14:05:08 -0400

    new ver info

--- jquery.multiselect.js
+++ jquery.multiselect.js
@@ -1,6 +1,6 @@
 /**
  * Display a nice easy to use multiselect list
- * @Version: 2.3.13
+ * @Version: 2.4
  * @Author: Patrick Springstubbe
  * @Contact: @JediNobleclem
  * @Website: springstubbe.us
